c48d2770171f51020fbb648e5d69675db2438d71
matthijsvanderplas <mvdplas84@ziggo.nl>
2023-03-08 13:45:31 +0100

    index.html update

--- test_main.py
+++ test_main.py
@@ -12,3 +12,5 @@ def test_homePage(client):
     response = client.get("/")
     assert response.status_code == 200
     assert b"<h1>Hello, world!</h1>" in response.data
+    assert b"<title>Home</title>" in response.data
+    assert b"<h2>I am now a DevOps Engineer!</h2>" in response.data
