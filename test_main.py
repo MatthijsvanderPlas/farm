21d9d3489d8815866c3a95ffd3e4ba7a3465dfe9
matthijsvanderplas <mvdplas84@ziggo.nl>
2023-03-08 14:56:41 +0100

    Final commit, all is working!

--- test_main.py
+++ test_main.py
@@ -13,4 +13,4 @@ def test_homePage(client):
     assert response.status_code == 200
     assert b"<h1>Hello, world!</h1>" in response.data
     assert b"<title>Home</title>" in response.data
-    assert b"<h2>I am now a DevOps Engineer</h2>" in response.data
+    assert b"<h2>I am now a DevOps Engineer!</h2>" in response.data
